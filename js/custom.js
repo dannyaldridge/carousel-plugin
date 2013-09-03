2e23a41ff36a26a8182e94a0181b794d224f78fe
Danny Aldridge <dannyaldridge@gmail.com>
2013-09-03 17:10:46 +0100

    Plugin with all appends and styling, non working using ''

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
     var carousel_css_left = parseInt($carousel_ul.css('left'));
     var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
     var left_indent = carousel_left - item_width;  
+    console.log(carousel_css_left);
 
     if ( index < $items.length -1) {
       $carousel_ul.animate({'left' : left_indent}, 500);  

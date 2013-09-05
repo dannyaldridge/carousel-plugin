9fb807d0277034ee2960487438b82fd13f3cb1d2
Danny Aldridge <dannyaldridge@gmail.com>
2013-09-05 17:13:24 +0100

    looped carousel

--- js/carouselplugin.js
+++ js/carouselplugin.js
@@ -8,7 +8,7 @@
             cssPrefix: 'da_'
           }, options);
 
-
+console.log(index);
 
       init = function () { 
         // TODO: prefix all class names, e.g. 'da_'
@@ -19,11 +19,11 @@
         $( ".carousel_container" ).prepend( '<div class="left_scroll"></div>' ).append( '<div class="right_scroll"></div>' );
         $(".left_scroll").append("<img src='pics/left.jpg' />").css({"float":"left","height":"10px","width":"15px","position":"relative","margin-top":"140px","margin-right":"7px"});;
         $(".right_scroll").append("<img src='pics/right.jpg' />").css({"float":"left","height":"10px","width":"15px","position":"relative","margin-top":"140px"});
-        // $(".carousel_inner").css({"float":"left","width":"400px","overflow":"hidden","height":"300px"});
         $(".carousel_ul").css({"position":"relative","list-style-type":"none","margin-top":"0px","padding":"0px","width":"9999px","padding-bottom":"10px"});
         // TODO: store arbitrary values in settings (e.g. 140px)
         $(".carousel_ul li").css({"float":"left","margin-left":"7px","width":"400px","padding":"0px","height":"110px","background":"#000000","margin-top":"10px","margin-bottom":"10px"});
-      
+    
+   
     $('.right_scroll img').click(function(){  
       var carousel_css_left = parseInt($element.css('left'));
       var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
@@ -35,7 +35,11 @@
         $element.animate({'left' : left_indent}, 500);  
         index += 1;
       } else {
-        // loop code
+       var carousel_css_left = parseInt($element.css('left'));
+      var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
+      var left_indent = carousel_left + item_width*5;
+      $element.animate({'left' : left_indent}, 500);  
+      index += 1;
       }
     });  
 
@@ -48,10 +52,13 @@
         $element.animate({'left' : left_indent}, 500);  
         index -= 1;
       } else {
-        // loop code
+      var carousel_css_left = parseInt($element.css('left'));
+      var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
+      var left_indent = carousel_left - item_width*5;
+      $element.animate({'left' : left_indent}, 500);  
+      index += 1;
       }
-
-    });  
+    }); 
     
 
   };

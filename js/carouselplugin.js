805115612f8c70578e12724ad9e2e3c836879423
Danny Aldridge <dannyaldridge@gmail.com>
2013-09-05 14:12:43 +0100

    ids changed to classes

--- js/carouselplugin.js
+++ js/carouselplugin.js
@@ -13,22 +13,22 @@
       init = function () {
         // TODO: move away from id's  
         // TODO: prefix all class names, e.g. 'da_'
-        $carouselContainer = $('<div id="carousel_container"></div>');
-        $element.wrap('<div id="carousel_inner" />');
+        $carouselContainer = $('<div class="carousel_container"></div>');
+        $element.wrap('<div class="carousel_inner" />');
 
-        $('#carousel_inner').wrap($carouselContainer);
-        $( "#carousel_container" ).prepend( '<div id="left_scroll"></div>' );
-        $( "#carousel_container" ).append( '<div id="right_scroll"></div>' );
-        $("#left_scroll").append("<img src='pics/left.jpg' />");
-        $("#right_scroll").append("<img src='pics/right.jpg' />");
-        $("#carousel_inner").css({"float":"left","width":"400px","overflow":"hidden","height":"300px"});
-        $("#carousel_ul").css({"position":"relative","list-style-type":"none","margin-top":"0px","padding":"0px","width":"9999px","padding-bottom":"10px"});
+        $('.carousel_inner').wrap($carouselContainer);
+        $( ".carousel_container" ).prepend( '<div class="left_scroll"></div>' );
+        $( ".carousel_container" ).append( '<div class="right_scroll"></div>' );
+        $(".left_scroll").append("<img src='pics/left.jpg' />");
+        $(".right_scroll").append("<img src='pics/right.jpg' />");
+        $(".carousel_inner").css({"float":"left","width":"400px","overflow":"hidden","height":"300px"});
+        $(".carousel_ul").css({"position":"relative","list-style-type":"none","margin-top":"0px","padding":"0px","width":"9999px","padding-bottom":"10px"});
         // TODO: store arbitrary values in settings (e.g. 140px)
-        $("#carousel_ul li").css({"float":"left","margin-left":"7px","width":"400px","padding":"0px","height":"110px","background":"#000000","margin-top":"10px","margin-bottom":"10px"});
-        $("#left_scroll").css({"float":"left","height":"10px","width":"15px","position":"relative","margin-top":"140px","margin-right":"7px"});
-        $('#right_scroll').css({"float":"left","height":"10px","width":"15px","position":"relative","margin-top":"140px"});
+        $(".carousel_ul li").css({"float":"left","margin-left":"7px","width":"400px","padding":"0px","height":"110px","background":"#000000","margin-top":"10px","margin-bottom":"10px"});
+        $(".left_scroll").css({"float":"left","height":"10px","width":"15px","position":"relative","margin-top":"140px","margin-right":"7px"});
+        $('.right_scroll').css({"float":"left","height":"10px","width":"15px","position":"relative","margin-top":"140px"});
       
-    $('#right_scroll').click(function(){  
+    $('.right_scroll').click(function(){  
       var carousel_css_left = parseInt($element.css('left'));
       var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
       var left_indent = carousel_left - item_width;
@@ -43,7 +43,7 @@
       }
     });  
 
-    $('#left_scroll img').click(function(){  
+    $('.left_scroll img').click(function(){  
       var carousel_css_left = parseInt($element.css('left'));
       var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
       var left_indent = carousel_left + item_width;  

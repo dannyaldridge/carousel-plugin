99a930116840eae51dc3b70cdd8c475e3df2b530
Adam Dawkins <adamdawkins@gmail.com>
2013-09-06 14:00:37 +0100

    Fixed scrolling bug, refactored code
    Refactored init() function into several smaller functions:
      setupStructure()
      setupStyling()
      next()
      previous()

--- js/carouselplugin.js
+++ js/carouselplugin.js
@@ -2,69 +2,78 @@
     $.fn.carouselplugin = function(options){
       var $element = $(this),  
           $items = $element.find('li'),
-          item_width = $items.outerWidth() + 10,
           index = 0,
-          settings = $.extend({
-            cssPrefix: 'da_'
-          }, options);
-
-console.log(index);
+          $carousel_inner,
+          $left_scroll,
+          $right_scroll;
 
       init = function () { 
-        // TODO: prefix all class names, e.g. 'da_'
-        $carouselContainer = $('<div class="carousel_container"></div>');
-        $element.wrap('<div class="carousel_inner" />');
-
-        $('.carousel_inner').css({"float":"left","width":"400px","overflow":"hidden","height":"300px"}).wrap($carouselContainer);
-        $( ".carousel_container" ).prepend( '<div class="left_scroll"></div>' ).append( '<div class="right_scroll"></div>' );
-        $(".left_scroll").append("<img src='pics/left.jpg' />").css({"float":"left","height":"10px","width":"15px","position":"relative","margin-top":"140px","margin-right":"7px"});;
-        $(".right_scroll").append("<img src='pics/right.jpg' />").css({"float":"left","height":"10px","width":"15px","position":"relative","margin-top":"140px"});
-        $(".carousel_ul").css({"position":"relative","list-style-type":"none","margin-top":"0px","padding":"0px","width":"9999px","padding-bottom":"10px"});
-        // TODO: store arbitrary values in settings (e.g. 140px)
-        $(".carousel_ul li").css({"float":"left","margin-left":"7px","width":"400px","padding":"0px","height":"110px","background":"#000000","margin-top":"10px","margin-bottom":"10px"});
+        setupStructure();
+        setupStyling();
     
-   
-    $('.right_scroll img').click(function(){  
-      var carousel_css_left = parseInt($element.css('left'));
-      var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
-      var left_indent = carousel_left - item_width;
-      // console.log($element);
+        $('.right_scroll img').click(function(){  
+          next();
+        });  
 
-   
-      if ( index < $items.length -1) {
+        $('.left_scroll img').click(function(){  
+          previous();
+        }); 
+    
+        return this;
+      },
+
+      setupStructure = function () {
+        $element.wrap('<div class="carousel_inner" />');
+        $carousel_inner = $('.carousel_inner'); 
+
+        $carousel_inner.wrap('<div class="carousel_container"></div>');
+
+        $('.carousel_container').prepend( '<div class="left_scroll"></div>' ).append( '<div class="right_scroll"></div>' );
+
+        $('.left_scroll').append('<img src="pics/left.jpg" />');
+        $('.right_scroll').append('<img src="pics/right.jpg" />');
+      },
+
+      setupStyling = function () {
+        $carousel_inner.css({'float':'left','width':'400px','overflow':'hidden','height':'300px'});
+        // TODO: come back to this
+        $('.left_scroll').css({'float':'left','height':'10px','width':'15px','position':'relative','margin-top':'140px','margin-right':'7px'});
+        $('.right_scroll').css({'float':'left','height':'10px','width':'15px','position':'relative','margin-top':'140px'});
+        $element.css({'position':'relative','list-style-type':'none','margin-top':'0px','padding':'0px','width':'9999px','padding-bottom':'10px', 'left': '0px'});
+        // TODO: store arbitrary values in settings (e.g. 140px)
+        $items.css({'float':'left','margin-left':'7px','padding':'0px','background':'#000000','margin-top':'10px','margin-bottom':'10px'});
+      },
+
+      next = function () {
+        var item_width = parseInt($($items[0]).outerWidth(), 10);
+        var carousel_left = parseInt($element.css('left'), 10);
+        var left_indent = carousel_left - (item_width + 10);
+     
+        if ( index < $items.length -1) {
+          $element.animate({'left' : left_indent}, 500);  
+          index += 1;
+        } else {
+          return false;
+        }
+      },
+
+      previous = function () {
+        var carousel_css_left = parseInt($element.css('left'), 10);
+        var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
+        var left_indent = carousel_left + item_width;  
+        
+        if (index > 0) {
+          $element.animate({'left' : left_indent}, 500);  
+          index -= 1;
+        } else {
+          carousel_css_left = parseInt($element.css('left'), 10);
+          carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
+          left_indent = carousel_left - item_width*5;
         $element.animate({'left' : left_indent}, 500);  
         index += 1;
-      } else {
-       var carousel_css_left = parseInt($element.css('left'));
-      var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
-      var left_indent = carousel_left + item_width*5;
-      $element.animate({'left' : left_indent}, 500);  
-      index += 1;
-      }
-    });  
+        }
+      };
 
-    $('.left_scroll img').click(function(){  
-      var carousel_css_left = parseInt($element.css('left'));
-      var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
-      var left_indent = carousel_left + item_width;  
-      
-      if (index > 0) {
-        $element.animate({'left' : left_indent}, 500);  
-        index -= 1;
-      } else {
-      var carousel_css_left = parseInt($element.css('left'));
-      var carousel_left = isNaN(carousel_css_left) ? 0 : carousel_css_left;
-      var left_indent = carousel_left - item_width*5;
-      $element.animate({'left' : left_indent}, 500);  
-      index += 1;
-      }
-    }); 
-    
-
-  };
-
-      // next = function () {}
-      // previous = function () {}
 
     init();
   }; 
